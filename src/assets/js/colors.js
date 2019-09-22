208cff8191192019869d7a5713fe51d052bd4f4c
Sid <siddharth@f22labs.com>
2019-09-22 20:01:18 +0530

    update colors and variables

---
+++ src/assets/js/colors.js
@@ -0,0 +1,9 @@
+const colors = {
+  $primaryWhite: `#FFF`,
+  $primaryGrey: `#ECEBF3`,
+  $primaryPurple: `#241B56`,
+  $primaryGreen: `#5AAE5D`,
+  $montserrat: `"Montserrat", sans-serif`
+};
+
+export default colors;
