d784f151f1c67a9f928b717c55a120986221716e
Sid <siddharth@f22labs.com>
2019-09-22 20:02:22 +0530

    add mock data for stats bar

---
+++ src/mock/MockData.js
@@ -0,0 +1,23 @@
+export const MockStats = {
+  MRR: {
+    value: "$84.92K",
+    tooltip: ""
+  },
+  "Active Subscriptions": {
+    value: "1,702",
+    tooltip: ""
+  },
+  "Net revenue": {
+    value: "$198.80K",
+    tooltip: ""
+  },
+  "Net payments received": {
+    value: "$194.80K",
+    tooltip: ""
+  },
+  "Unpaid invoices": {
+    value: "80",
+    suffix: "$2.34K",
+    tooltip: ""
+  }
+};
